48f16f312d5017ca6a29508037bc5069b5d854b8
pduvvur <pradeepduvvur@gmail.com>
2015-10-31 14:30:11 -0400

    Removed some comments

--- app/src/main/java/com/pduvvur/saveme/LocationSMSService.java
+++ app/src/main/java/com/pduvvur/saveme/LocationSMSService.java
@@ -90,17 +90,7 @@ public class LocationSMSService extends Service implements
 
         @Override
         public void handleMessage(Message msg) {
-            // Normally we would do some work here, like download a file.
-            // For our sample, we just sleep for 5 seconds.
-            /*long endTime = System.currentTimeMillis() + 5 * 1000;
-            while (System.currentTimeMillis() < endTime) {
-                synchronized (this) {
-                    try {
-                        wait(endTime - System.currentTimeMillis());
-                    } catch (Exception e) {
-                    }
-                }
-            }*/ // TODO : delete
+
             m_googleApiClient.blockingConnect(2000, TimeUnit.MILLISECONDS);
             // Stop the service using the startId, so that we don't stop
             // the service in the middle of handling another job
